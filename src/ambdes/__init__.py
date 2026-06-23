1657cee64ff63d98aee41868bcafeba0ea73661d
amyheather <a.heather2@exeter.ac.uk>
2026-06-23 15:42:12 +0100

    chore(animation): remove generate_animation function for now

--- src/ambdes/__init__.py
+++ src/ambdes/__init__.py
@@ -5,7 +5,6 @@ __version__ = "0.1.0"
 # This section allows us to import using e.g. `from ambdes import Model`,
 # rather than `from ambdes.model import Model`.
 
-from .animation import generate_animation
 from .choose_warm_up import WarmUpAuditor, plot_warm_up, run_warm_up_audit
 from .config import ArrivalConfig, SimConfig, TimesConfig
 from .model import Model
@@ -15,7 +14,6 @@ from .runner import Runner
 
 __all__ = [
     "ArrivalConfig",
-    "generate_animation",
     "Model",
     "Patient",
     "plot_warm_up",
